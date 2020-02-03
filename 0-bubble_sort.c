41ddb909607d4708af170c68966f5346f72d15d5
oran2527 <orlago250183@gmail.com>
2020-02-03 20:04:31 +0000

    point 0 fix null

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,8 +9,8 @@ void bubble_sort(int *array, size_t size)
 	int next, prev, flag = 0;
 	size_t cont, j, i;
 
-	if (size <= 1)
-		print_array(array, size);
+	if (array == NULL || size <= 1)
+		return;
 	if (size > 1)
 	{
 		for (i = 0; flag == 0; i++)
